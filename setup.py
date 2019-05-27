ab7589f76945046bddff03e9b325b147ac104e3d
Yvictor <yvictor3141@gmail.com>
2019-05-27 23:42:51 +0800

    fixed for python3

--- setup.py
+++ setup.py
@@ -3,14 +3,20 @@ from setuptools import setup
 
 setup(
     name='wssh',
-    version='0.1.0',
-    author='Andrea Luzzardi <aluzzardi@gmail.com>',
+    version='0.1.1',
+    author='Andrea Luzzardi <aluzzardi@gmail.com>, Yvictor',
     packages=[
         'wssh'
         ],
     scripts=[
         'bin/wssh',
         'bin/wsshd'
+        ],
+    install_requires=[
+        'gevent', 
+        'flask', 
+        'paramiko', 
+        'gevent-websocket',
         ],
     package_data={'': ['static/*', 'templates/*']},
     include_package_data=True,
